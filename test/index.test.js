2875e6c19ff50d081d3cb628f5682d99e7d02fb8
Michele Palamidessi <michele.palamidessi@protonmail.com>
2022-10-14 23:04:55 +0200

    feat: add possibility to add new processors

--- test/index.test.js
+++ test/index.test.js
@@ -27,7 +27,15 @@ t.test('It should be at least 1 eligible attributes', async (t) => {
 t.test('It should add attribute as eligible in stats module', async (t) => {
   const stats = await nftCollectionStats({
     source: { type: 'xxx' },
-    collection: { attributes: ['field1', 'field2'] },
+    collection: { attributes: ['field1', 'field2'], supply: 1 },
   })
   t.equals(2, Array.from(stats.getCollection().attributesMap().keys()).length)
+})
+
+t.test('It should set supply to the collection', async (t) => {
+  const stats = await nftCollectionStats({
+    source: { type: 'xxx' },
+    collection: { attributes: ['field1', 'field2'], supply: 131 },
+  })
+  t.equals(stats.getCollection().supply(), 131)
 })
